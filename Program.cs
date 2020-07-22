2af88e548d48c2ba8195cd0a206e3ca7d1103504
MechaDragonX <theofficialrv@gmail.com>
2020-07-21 22:28:05 -0700

    Fix alt text parsing

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace TextToSubStationAlpha
             string endTime = "09:27";
             string[] current;
             string[] next = null;
+            bool alt = false;
             string time1 = "";
             string time2;
             string line1;
@@ -31,31 +32,46 @@ namespace TextToSubStationAlpha
                 if(i != text.Length - 1)
                     next = text[i + 1].Split('\t');
 
-                time1 = onscreenPattern.Match(current[0]).Captures[0].Value;
-                if (next != null)
-                {   
-                    time2 = onscreenPattern.Match(next[0]).Captures[0].Value;
+                if(current[0].Contains(' '))
+                    alt = true;
 
-                    line1 = $"Dialogue: 0,0:{time1}.00,0:{time2}.00,Default,,0,0,0,,{{\\be2}}{current[1]}";
-                    if (current[2] != "")
-                        line2 = $"Dialogue: 0,0:{time1}.00,0:{time2}.00,Notes,,0,0,0,,{{\\be2}}{current[2]}";
+                time1 = onscreenPattern.Match(current[0]).Captures[0].Value;
+                if(next != null)
+                {
+                    time2 = onscreenPattern.Match(next[0]).Captures[0].Value;
+                    if(alt)
+                    {
+                        line1 = $"Dialogue: 0,0:{time1}.00,0:{time2}.00,Alt Dialogue,,0,0,0,,{{\\be2}}{current[1]}";
+                        if(current[2] != "")
+                            line2 = $"Dialogue: 0,0:{time1}.00,0:{time2}.00,Notes,,0,0,0,,{{\\be2}}{current[2]}";
+                    }
+                    else
+                    {
+                        line1 = $"Dialogue: 0,0:{time1}.00,0:{time2}.00,Default,,0,0,0,,{{\\be2}}{current[1]}";
+                        if (current[2] != "")
+                            line2 = $"Dialogue: 0,0:{time1}.00,0:{time2}.00,Notes,,0,0,0,,{{\\be2}}{current[2]}";
+                    }
                 }
                 else
                 {
-                    line1 = $"Dialogue: 0,0:{time1}.00,0:{endTime}.00,Default,,0,0,0,,{{\\be2}}{current[1]}";
-                    if (current[2] != "")
-                        line2 = $"Dialogue: 0,0:{time1}.00,0:{endTime}.00,Notes,,0,0,0,,{{\\be2}}{current[2]}";
+                    if(alt)
+                    {
+                        line1 = $"Dialogue: 0,0:{time1}.00,0:{endTime}.00,Alt Dialogue,,0,0,0,,{{\\be2}}{current[1]}";
+                        if(current[2] != "")
+                            line2 = $"Dialogue: 0,0:{time1}.00,0:{endTime}.00,Notes,,0,0,0,,{{\\be2}}{current[2]}";
+                    }
+                    else
+                    {
+                        line1 = $"Dialogue: 0,0:{time1}.00,0:{endTime}.00,Default,,0,0,0,,{{\\be2}}{current[1]}";
+                        if(current[2] != "")
+                            line2 = $"Dialogue: 0,0:{time1}.00,0:{endTime}.00,Notes,,0,0,0,,{{\\be2}}{current[2]}";
+                    }
                 }
 
                 if(line2 != "")
                     File.AppendAllLinesAsync(writePath, new string[] { line1, line2 });
                 else
                     File.AppendAllLinesAsync(writePath, new string[] { line1 });
-
-                if (line2 == "")
-                    Console.WriteLine(line1);
-                else
-                    Console.WriteLine(line1 + "\n" + line2);
 
                 line1 = "";
                 line2 = "";
